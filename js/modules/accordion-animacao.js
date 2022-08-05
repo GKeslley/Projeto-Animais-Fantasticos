af51af66c7e946695fd669f2b4afa44dad2793ec
Guilherme Keslley <guilhermekeslley123@gmail.com>
2022-08-05 16:29:28 -0300

    Refatoração accordion

--- js/modules/accordion-animacao.js
+++ js/modules/accordion-animacao.js
@@ -1,20 +1,24 @@
-export default function initAccordion() {
-  const accordionList = document.querySelectorAll(
-    "[data-anime='accordion'] dt"
-  );
-  const activeClass = "ativo";
-
-  function accordionActive() {
-    this.classList.toggle(activeClass);
-    this.nextElementSibling.classList.toggle(activeClass);
+export default class Accordion {
+  constructor(list) {
+    this.accordionList = document.querySelectorAll(list);
+    this.activeClass = "ativo";
   }
 
-  if (accordionList.length) {
-    accordionList[0].classList.add(activeClass);
-    accordionList[0].nextElementSibling.classList.add(activeClass);
-
-    accordionList.forEach((item) => {
-      item.addEventListener("click", accordionActive);
+  addAcorddionEvent() {
+    this.accordionList.forEach((item) => {
+      item.addEventListener("click", () => this.accordionToggle(item));
     });
+  }
+
+  accordionToggle(item) {
+    item.classList.toggle(this.activeClass);
+    item.nextElementSibling.classList.toggle(this.activeClass);
+  }
+
+  init() {
+    if (this.accordionList.length) {
+      this.accordionToggle(this.accordionList[0]);
+      this.addAcorddionEvent();
+    }
   }
 }
