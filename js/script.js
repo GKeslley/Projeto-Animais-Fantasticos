676935da113922a3505426b380fc059b5d14ebf0
Guilherme Keslley <guilhermekeslley123@gmail.com>
2022-08-08 15:48:51 -0300

    Refatoração modal

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ import ScrollSuave from "./modules/scroll-suave.js";
 import initAnimacao from "./modules/scroll-animacao.js";
 import Accordion from "./modules/accordion-animacao.js";
 import Tabnav from "./modules/tab-nav.js";
-import initModal from "./modules/modal.js";
+import Modal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
@@ -24,7 +24,14 @@ const tabNav = new Tabnav(
 );
 tabNav.init();
 
-initModal();
+const modal = new Modal(
+  "[data-modal='abrir']",
+  "[data-modal='container']",
+  '[data-modal="fechar"]'
+);
+
+modal.init();
+
 initTooltip();
 initDropdownMenu();
 initMenuMobile();
