10aa3db7985dd42032b64393ef1ff8223e83b3fa
Guilherme Keslley <guilhermekeslley123@gmail.com>
2022-08-17 16:31:32 -0300

    Funcionamneto refatorado

--- js/modules/funcionamento.js
+++ js/modules/funcionamento.js
@@ -1,17 +1,43 @@
-export default function initFuncionamento() {
-  const funcionamento = document.querySelector("[data-semana]");
-  const diasSemana = funcionamento.dataset.semana.split(",").map(Number);
-  const horarioSemana = funcionamento.dataset.horario.split(",").map(Number);
+export default class Funcionamento {
+  constructor(funcionamento, activeClass) {
+    this.funcionamento = document.querySelector(funcionamento);
+    this.activeClass = activeClass;
+  }
 
-  const diaSemana = new Date();
-  const diaAgora = diaSemana.getDay();
-  const horarioAgora = diaSemana.getHours();
+  dadosFuncionamento() {
+    this.diasSemana = this.funcionamento.dataset.semana.split(",").map(Number);
+    this.horarioSemana = this.funcionamento.dataset.horario
+      .split(",")
+      .map(Number);
+  }
 
-  const semanaAberto = diasSemana.indexOf(diaAgora) !== -1;
-  const horarioAberto =
-    horarioAgora >= horarioSemana[0] && horarioAgora < horarioSemana[1];
+  dadosAgora() {
+    this.diaSemana = new Date();
+    this.diaAgora = this.diaSemana.getDay();
+    this.horarioAgora = this.diaSemana.getUTCHours() - 3;
+  }
 
-  if (semanaAberto && horarioAberto) {
-    funcionamento.classList.add("aberto");
+  estaAbertoAgora() {
+    this.semanaAberto = this.diasSemana.indexOf(this.diaAgora) !== -1;
+    this.horarioAberto =
+      this.horarioAgora >= this.horarioSemana[0] &&
+      this.horarioAgora < this.horarioSemana[1];
+
+    return this.semanaAberto && this.horarioAberto;
+  }
+
+  ativaAberto() {
+    if (this.estaAbertoAgora()) {
+      this.funcionamento.classList.add(this.activeClass);
+    }
+  }
+
+  init() {
+    if (this.funcionamento) {
+      this.dadosFuncionamento();
+      this.dadosAgora();
+      this.ativaAberto();
+    }
+    return this;
   }
 }
